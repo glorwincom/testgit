7aafdf2c0369c10d079da06e869529a8df324fb5
Wing <Wing@qq.com>
2022-05-03 19:48:16 +0800

    second commit

--- src/git01/test01.java
+++ src/git01/test01.java
@@ -3,5 +3,7 @@ package git01;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        int a=4;
+        System.out.println(a);
     }
 }
